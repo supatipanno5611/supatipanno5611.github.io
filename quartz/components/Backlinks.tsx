c3404223c897e2cd6b09a8d8a7fcacc4bd450479
dosakkhayo <supatipanno5611@gmail.com>
2026-02-24 08:06:11 +0900

    오른쪽 사이드바 모바일에서 그래프 말고 다른 것도 나오게

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -10,7 +10,7 @@ interface BacklinksOptions {
 }
 
 const defaultOptions: BacklinksOptions = {
-  hideWhenEmpty: true,
+  hideWhenEmpty: false,
 }
 
 export default ((opts?: Partial<BacklinksOptions>) => {
