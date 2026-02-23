eb911613f6336099f2094984bb6d70a2096c114d
dosakkhayo <supatipanno5611@gmail.com>
2026-02-24 08:48:51 +0900

    작성일과 수정일 모두 표기

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -29,8 +29,11 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
     if (text) {
       const segments: (string | JSX.Element)[] = []
 
-      if (fileData.dates) {
-        segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
+      if (fileData.dates?.created) {
+        segments.push(<span>작성: <Date date={fileData.dates.created} locale={cfg.locale} /></span>)
+      }
+      if (fileData.dates?.modified) {
+        segments.push(<span>수정: <Date date={fileData.dates.modified} locale={cfg.locale} /></span>)
       }
 
       // Display reading time if enabled
